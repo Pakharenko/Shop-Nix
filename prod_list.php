69e86e9c9d4ecb17cbffcaae81c7e22be0bab678
Pakharenko <paharenko1985@mail.ru>
2018-01-10 17:05:55 +0200

    Update prod_list.php

--- prod_list.php
+++ prod_list.php
@@ -1,10 +1,9 @@
 <?php
 
-<<<<<<< HEAD
-return $products = [
-=======
+
+
  return $products = [
->>>>>>> bce615119ddcaa5a1b6410684c6db92b3f39b25f
+
     [
         'id' => 0,
         'title' => 'Iphone',
@@ -26,7 +25,7 @@ return $products = [
         'price' => '22.10',
         'count_goods' => 8,
     ],
-<<<<<<< HEAD
+
 //    [
 //        'id' => 3,
 //        'title' => 'Apple',
@@ -48,27 +47,5 @@ return $products = [
 //        'price' => '750.00',
 //        'count_goods' =>12,
 //    ],
-=======
-    [
-        'id' => 3,
-        'title' => 'Apple',
-        'desc' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque dolores doloribus eum fuga incidunt ipsa maiores',
-        'price' => '1000.55',
-        'count_goods' => 91,
-    ],
-    [
-        'id' => 4,
-        'title' => 'Samsung',
-        'desc' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque dolores doloribus eum fuga incidunt ipsa maiores',
-        'price' => '88.45',
-        'count_goods' => 2,
-    ],
-    [
-        'id' => 5,
-        'title' => 'Nokia',
-        'desc' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque dolores doloribus eum fuga incidunt ipsa maiores',
-        'price' => '750.00',
-        'count_goods' =>12,
-    ],
->>>>>>> bce615119ddcaa5a1b6410684c6db92b3f39b25f
+
 ];
